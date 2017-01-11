d9f8abcefe439fb7cda2fbd16d933dbe697065b9
darkmoon <505916481@qq.com>
2017-01-11 15:35:44 +0800

    First Commit

---
+++ README.md
@@ -0,0 +1,18 @@
+Vendor blobs for Huawei h30_t10
+---------------
+
+已修复GPS和录屏等
+heyuhui分享
+
+
+* Compilation
+
+        # repo init -u git://github.com/ferhung-mtk/android.git -b cm-13.0
+        
+        # repo sync
+        
+        # source build/envsetup.sh
+        
+        # brunch cm_h30_t10-userdebug
+
+---------------
---
+++ h30_t10-vendor.mk
@@ -0,0 +1,11 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Huawei/h30_t10/proprietary/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Huawei/h30_t10/proprietary/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Huawei/h30_t10/proprietary/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Huawei/h30_t10/proprietary/xbin,system/xbin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Huawei/h30_t10/proprietary/vendor,system/vendor)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Huawei/h30_t10/proprietary/priv-app,system/priv-app)
